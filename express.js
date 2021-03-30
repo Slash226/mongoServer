fab559122074e11d25927f21741815a0aeaffbfb
suezp <suzhipeng@zjmx.com>
2021-03-30 18:04:48 +0800

    解决同端口页面 axios问题

--- express.js
+++ express.js
@@ -15,6 +15,7 @@ mongoose.connection.once("open", () => {
 })
 
 app.use(express.static(path.join(__dirname,'static/images')))
+app.use(express.static(path.join(__dirname,'src')))
 app.use(express.static(path.join(__dirname,'pages')))
 // app.use(express.static(path.join(__dirname, 'static')))
 console.log(path.join(__dirname,'static'))
